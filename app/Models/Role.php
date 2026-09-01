b6415a6e90112c72051d48b3bb0b085a131f8f7b
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2026-09-01 22:34:52 +0600

    feat(admin): protect admin and add property approval

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -1,10 +1,8 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
-
-class Role extends Model
-{
-    protected $fillable = ['name','slug','description'];
+use Illuminate\Database\Eloquent\Relations\HasMany;
+class Role extends Model {
+ protected $fillable=['name','slug','description'];
+ public function assignments():HasMany{return $this->hasMany(UserRole::class);}
 }
