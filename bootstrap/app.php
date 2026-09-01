938cc100d6fd7abc579ad12cce1595fb6f980a01
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2026-09-02 00:33:31 +0600

    fix(laravel): correct admin middleware namespace syntax

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias(['admin' => \\App\\Http\\Middleware\\EnsureAdmin::class]);
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\EnsureAdmin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        // Exception reporting configuration will be added here.
+        //
     })->create();
