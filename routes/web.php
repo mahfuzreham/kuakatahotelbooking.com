a79898110e6750a66b0cc2d1f37dae13eb51171f
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2026-09-03 16:57:58 +0600

    feat(admin): add notification center routes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\Admin\BookingController as AdminBookingController;
 use App\Http\Controllers\Admin\RefundController as AdminRefundController;
 use App\Http\Controllers\Admin\FinanceWebController;
 use App\Http\Controllers\Admin\VerificationWebController;
+use App\Http\Controllers\Admin\NotificationWebController;
 
 Route::get('/', fn () => view('home'))->name('home');
 Route::get('/search', [HotelSearchController::class, 'index'])->name('hotels.search');
@@ -42,6 +43,8 @@ Route::middleware('auth')->group(function(){
  Route::post('/admin/users/{user}/roles',[UserController::class,'updateRoles'])->name('admin.users.roles');
  Route::get('/admin/bookings',[AdminBookingController::class,'index'])->name('admin.bookings.index');
  Route::post('/admin/bookings/{booking}/status',[AdminBookingController::class,'updateStatus'])->name('admin.bookings.status');
+ Route::get('/admin/notifications',[NotificationWebController::class,'index'])->name('admin.notifications.index');
+ Route::post('/admin/notifications/send',[NotificationWebController::class,'send'])->name('admin.notifications.send');
  Route::get('/admin/verifications',[VerificationWebController::class,'index'])->name('admin.verifications.index');
  Route::post('/admin/verifications/{verification}/approve',[VerificationWebController::class,'approve'])->name('admin.verifications.approve');
  Route::post('/admin/verifications/{verification}/reject',[VerificationWebController::class,'reject'])->name('admin.verifications.reject');
